54a43addc6bdc80a3a65ddb5003fc0c8a6ab1872
Andrew <oi.6898@mail.ru>
2024-04-09 17:06:25 +0300

    add ci-cd

--- exchange-rate-adapter/src/main/java/io/github/bank/exchangerate/adapter/client/ExchangeRateApiClient.java
+++ exchange-rate-adapter/src/main/java/io/github/bank/exchangerate/adapter/client/ExchangeRateApiClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "exchangeApiClient", url = "${adapter.client.exchange-rate.url}")
 public interface ExchangeRateApiClient {
     //https://api.fxratesapi.com/latest?base=USD&format=json
-    @GetMapping(value = "test?api_key=9d16deaa1b898fdc43d3e23b7d6&base={currency}&format=json")
+    @GetMapping(value = "latest?api_key=9d16deaa1b898fdc43d3e23b7d6&base={currency}&format=json")
     ExchangeRateApiResponse getActualRates(@PathVariable String currency);
 //"https://api.fxratesapi.com/historical?api_key=fxr_live_caae3d32b9d16deaa1b898fdc43d3e23b7d6&date=2020-01-01&base=USD"
     @GetMapping(value = "historical?date={year}-{month}-{day}&base={currency}")
@@ -21,7 +21,7 @@ public interface ExchangeRateApiClient {
 
     @GetMapping(value = "convert?{apiKey}&from={fromCurrency}&to={fromCurrency}&amount={amount}}")
     ExchangeRateApiResponse convert(@PathVariable String apiKey,
-                                           @PathVariable String fromCurrency,
-                                           @PathVariable String toCurrency,
-                                           @PathVariable Double amount);
+                                    @PathVariable String fromCurrency,
+                                    @PathVariable String toCurrency,
+                                    @PathVariable Double amount);
 }
